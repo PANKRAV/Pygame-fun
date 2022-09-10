5fe20cc124120d6ce731ecff2e7e28f7baa73e77
PANKRAV <pankravaris@gmail.com>
2022-09-11 01:43:21 +0300

    laid the groundwork for the terrain obejects

--- scripts/terrain.py
+++ scripts/terrain.py
@@ -1,1 +1,54 @@
 import pygame
+
+
+class Terrain:
+    def __init__(self):
+        pass
+
+
+
+
+class Platorm(Terrain):
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+class Moving_Platform(Platorm):
+    def __init__(self):
+        pass
+
+
+
+
+
+class Wall(Terrain):
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+class Sticky_Wall(Wall):
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+class Slope(Terrain):
+    def __init__(self):
+        pass
