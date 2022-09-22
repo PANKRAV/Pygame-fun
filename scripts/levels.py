3895ebf0e9eb71a49287b9e0738541bf0f916413
PANKRAV <pankravaris@gmail.com>
2022-09-22 18:01:03 +0300

    I wrote the Level.load(), Level.run() methods. Haven't tested them yet

--- scripts/levels.py
+++ scripts/levels.py
@@ -6,6 +6,7 @@ import pathlib
 import os
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from scripts.sprites import Player
 
 
 import terrain as t
@@ -21,21 +22,47 @@ class Level:
     
     current = 0
 
-    def __init__(self, platforms : list[t.Platform], enemies : list[sprites.Enemy], goal : list[t.Goal], buttons : list[gui.Button], start : tuple[int]):
-        self.platfroms = platforms
-        self.enemies = enemies
+    def __init__(self, platforms : list[t.Platform], enemies : list[sprites.Enemy], goal : t.Goal, buttons : list[gui.Button], player : sprites.Player):
+        self.platfroms : list[t.Platform] = platforms
+        self.enemies : list[sprites.Enemy] = enemies
         self.goal = goal
-        self.start = start
-        self.buttons = buttons
-        self.num = 0
+        self.player : sprites.Player = player
+        self.buttons : list[gui.Button] = buttons
+        self.num = Level.current
+
+
+        Level.current += 1
 
 
 
 
 
+    def run(self):
+        self.player.plat_check()
+        self.player.update(v.screen)
+        self.player.goal_check(self.goal)
+        self.player.draw()
 
-    def setup(self):
-        ...
+
+        self.player.life_check()
+        self.player.life_draw()
+
+
+
+        for platform in self.platfroms :
+            platform.update(self.player)
+            platform.draw(v.screen)
+
+
+        for enemy in self.enemies :
+            enemy.update()
+            enemy.draw(v.screen)
+
+
+        for button in self.buttons :
+            button.update()
+            button.draw(v.screen)
+            
 
 
     def end(self):
@@ -48,7 +75,12 @@ class Level:
 
     @classmethod
     def load(cls, _json : pathlib.Path) -> Level :
-        
+        platforms = []
+        enemies = []
+        buttons = []
+
+
+
         with _json.open(mode = "rt") as f :
             _json = f.read()
             _json = json.loads(_json)
@@ -58,20 +90,24 @@ class Level:
         Goal = t.Goal((_json["goal"]["x"], _json["goal"["y"]]), _json["goal"]["size"], v.GREEN)
 
 
-        for enemy in _json["enemy"] :
-            if isinstance(enemy, sprites.Moving_Enemy ) :
-                pass
+        for enemy in _json["enemies"] :
+            for moving_enemy in enemy["moving_enemies"] :
+                enemies.append(sprites.Moving_Enemy((moving_enemy["x"], moving_enemy["y"]), moving_enemy["u"]))
 
-            else :
-                pass
+            for norm_enemy in enemy["norm_enemies"] :
+                enemies.append(sprites.Enemy((norm_enemy["x"], norm_enemy["y"])))
+
+
+            for air_enemy in enemy["air_enemies"] :
+                ...
 
 
         for terrain in _json["terrain"] :
             for platform in terrain["platform"] :
-                pass
+                platforms.append(t.Platform(platform["x"], platform["y"], platform["width"], platform["height"], platform["color"], platform["μ"], platform["solid"], platform["rounded"]))
 
-            for moving_platform in terrain["moving_platform"] :
-                pass
+            for platform in terrain["moving_platform"] :
+                platforms.append(t.Moving_Platform(platform["x"], platform["y"], platform["width"], platform["height"], platform["color"], platform["μ"], platform["ux"], platform["uy"], platform["travelx"], platform["travely"], platform["solid"], platform["rounded"], platform["player_required"], platform["follow_player"]))
 
             for bouncer in terrain["bouncer"] :
                 pass
@@ -93,6 +129,10 @@ class Level:
         v.screen_color = env_data["background_color"]
         v.g = env_data["g"]
         v.game_speed = env_data["game_speed"]
+
+
+
+        return cls(platforms, enemies, Goal, buttons, Player)
 
 
 
